96e9c44bb6d780f4c8e9b68ea5cb2aada6400f9e
ronenpi18 <ronenpi18@gmail.com>
2018-05-06 20:35:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	'secret': 'psstappisviral',
-    'database' : 'mongodb://ronenpi18:QWeasd123!@cluster0-shard-00-00-5n32v.mongodb.net:27017,cluster0-shard-00-01-5n32v.mongodb.net:27017,cluster0-shard-00-02-5n32v.mongodb.net:27017/psst?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+    'database' : 'mongodb://username:password!@cluster0-shard-00-00-5n32v.mongodb.net:27017,cluster0-shard-00-01-5n32v.mongodb.net:27017,cluster0-shard-00-02-5n32v.mongodb.net:27017/psst?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 };
